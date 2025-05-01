3cfccf8742c999ab0b420756f71f3086646d54bb
traaitt <traaitt@icloud.com>
2025-05-01 02:58:56 +0200

    Revert "Update GetBlockchainInfoResponse.cs"
    This reverts commit 9e9b8ad7365c637707886d2c5532ae33e44f6d62.

--- src/Miningcore/Blockchain/Bitcoin/DaemonResponses/GetBlockchainInfoResponse.cs
+++ src/Miningcore/Blockchain/Bitcoin/DaemonResponses/GetBlockchainInfoResponse.cs
@@ -10,5 +10,4 @@ public class BlockchainInfo
     public long MedianTime { get; set; }
     public double VerificationProgress { get; set; }
     public bool Pruned { get; set; }
-    public bool initialblockdownload { get; set; }
 }
